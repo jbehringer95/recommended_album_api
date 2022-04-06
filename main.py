e5ecb2e78f205c053977d209fffaa9cf76b52082
Justin C Behringer <63168114+jbehringer95@users.noreply.github.com>
2022-04-05 20:16:08 -0500

    Getting code ready for EB

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import re
 import json
 import urllib.parse
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from flask import Flask
 from flask_restful import Api, Resource
 from predictions import get_recommendations
@@ -12,6 +14,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SPOTIFY_ID = os.getenv('spotify_client_id')
+SPOTIFY_SECRET = os.getenv('spotify_client_secret')
+
 ACCESS_KEY = os.getenv('access_key')
 SECRET_KEY = os.getenv('secret_key')
 
@@ -21,18 +26,33 @@ api = Api(app)
 
 class AlbumSuggester(Resource):
     def get(self, value_list):
+        client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_ID, client_secret=SPOTIFY_SECRET)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
         value_list = urllib.parse.unquote(value_list)
         value_list = value_list.split(', ')
         value_list = [re.sub('[^a-zA-Z\s]', '', i) for i in value_list]
         df = pulling_dataframe(ACCESS_KEY, SECRET_KEY)
         recommendations = get_recommendations(df, 'Album', 'New_Album', value_list)
+
         album = []
-        for num in range(1,6):
+        album_names = []
+
+        for num in range(1,10):
+            # Pulling the album data from the database one at a time
             response = query_album(int(recommendations['index'].iloc[num]), ACCESS_KEY, SECRET_KEY)
 
             for values in response:
-                del values['Index']
-                album.append(values)
+
+                if values['Album_Name'] not in album_names:
+                    album_names.append(values['Album_Name'])
+                    del values['Index']
+                    album_result = sp.search(values['Album_Name'], type='album', limit=1)
+                    values['url'] = album_result['albums']['items'][0]['images'][0]['url']
+                    album.append(values)
+            
+            if len(album) == 5:
+                break
 
         return album
 
--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  profile = "shuler-io"
-}
-
-resource "aws_s3_bucket" "predict-s3" {
-  bucket = "shuler-consulting-music-predict"
-  acl    = "public-read-write"
-}
