5651564e84fae7c9e4a6936c7accc041fcdff647
Justin Behringer <justin.caise95@gmail.com>
2025-03-17 18:49:59 -0500

    Removed frontend components, updated to backend-only API

--- album_recommendation_website/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as SearchBar} from './SearchBar/SearchBar'
-export {default as SearchResults} from './SearchResults/SearchResults'
-export {default as SelectedTags} from './SelectedTags/SelectedTags'
